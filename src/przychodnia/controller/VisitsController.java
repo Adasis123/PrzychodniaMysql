c48d36c80865f5cf6ed418f6870dc18483fdc293
Adam Danielczyk <adam.danielczyk@performgroup.com>
2017-01-05 23:13:52 +0100

    Add lambda into VisitsController

--- src/przychodnia/controller/VisitsController.java
+++ src/przychodnia/controller/VisitsController.java
@@ -6,16 +6,12 @@ import com.jfoenix.controls.RecursiveTreeItem;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableColumn;
 import javafx.scene.layout.AnchorPane;
-import javafx.util.Callback;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -34,30 +30,15 @@ public class VisitsController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         JFXTreeTableColumn<Visit, String> pName = new JFXTreeTableColumn<>("Pacjent");
         pName.setPrefWidth(150);
-        pName.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Visit, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Visit, String> param) {
-                return param.getValue().getValue().pName;
-            }
-        });
+        pName.setCellValueFactory(param -> param.getValue().getValue().pName);
 
         JFXTreeTableColumn<Visit, String> dName = new JFXTreeTableColumn<>("Lekarz");
         dName.setPrefWidth(150);
-        dName.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Visit, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Visit, String> param) {
-                return param.getValue().getValue().dName;
-            }
-        });
+        dName.setCellValueFactory(param -> param.getValue().getValue().dName);
 
         JFXTreeTableColumn<Visit, String> vDate = new JFXTreeTableColumn<>("Data Wizyty");
         vDate.setPrefWidth(145);
-        vDate.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<Visit, String>, ObservableValue<String>>() {
-            @Override
-            public ObservableValue<String> call(TreeTableColumn.CellDataFeatures<Visit, String> param) {
-                return param.getValue().getValue().vDate;
-            }
-        });
+        vDate.setCellValueFactory(param -> param.getValue().getValue().vDate);
 
         ObservableList<Visit> users = FXCollections.observableArrayList();
         users.add(new Visit("Adam Danielczyk", "Ryszard Zieliński", "12/12/2017"));
@@ -70,6 +51,7 @@ public class VisitsController implements Initializable {
         visitsTree.setShowRoot(false);
 
     }
+
 
     class Visit extends RecursiveTreeObject<Visit> {
 
