f01a660d740f81232126841b039c042dbf85754c
Adam Danielczyk <adam.danielczyk@performgroup.com>
2017-01-05 09:27:48 +0100

    Add start Application

--- src/przychodnia/Main.java
+++ src/przychodnia/Main.java
@@ -1,7 +1,43 @@
 package przychodnia;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * Created by adam on 04/01/2017.
  */
-public class Main {
+public class Main extends Application {
+
+    private Stage primaryStage;
+    private static StackPane rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Przychodnia");
+        initRootLayout();
+
+    }
+
+    public void initRootLayout() throws IOException {
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("view/RootLayout.fxml"));
+        rootLayout = (StackPane) loader.load();
+        Scene scene = new Scene(rootLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ src/przychodnia/controller/RootLayoutController.java
@@ -0,0 +1,23 @@
+package przychodnia.controller;
+
+/**
+ * Created by adam on 05/01/2017.
+ */
+public class RootLayoutController {
+
+    public void goExit() {
+        System.exit(0);
+    }
+
+    public void goVisits() {
+
+    }
+
+    public void goPatients() {
+
+    }
+
+    public void goDoctors() {
+
+    }
+}
