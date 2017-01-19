4e132f242bcf1ee22e0105fce56c9cadb1243b5c
Adam Danielczyk <adam.danielczyk@performgroup.com>
2017-01-19 12:16:55 +0100

    Add validation

--- src/przychodnia/controller/AddPatientController.java
+++ src/przychodnia/controller/AddPatientController.java
@@ -1,6 +1,7 @@
 package przychodnia.controller;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.validation.RequiredFieldValidator;
 import javafx.fxml.FXML;
@@ -18,6 +19,8 @@ import java.util.ResourceBundle;
  */
 public class AddPatientController implements Initializable {
 
+    @FXML
+    private JFXTextArea patientValidator;
 
     @FXML
     public JFXButton addPatientBtn;
@@ -44,7 +47,7 @@ public class AddPatientController implements Initializable {
     private JFXTextField pPesel;
 
     @FXML
-    public void cancelPatient() throws IOException {
+    private void cancelPatient() throws IOException {
         if (Main.isAddPatientModal()) {
             Main.getModalPatient().close();
             Main.setAddPatientModal(false);
@@ -54,50 +57,130 @@ public class AddPatientController implements Initializable {
 
     @FXML
     public void addPatient() throws SQLException, ClassNotFoundException, IOException {
+        val();
+        if (!checkPatientFields()){
         if (Main.isAddPatientModal()) {
-            PatientsDAO.insertPatient(pSurname.getText(), pName.getText(), pCity.getText(), pZipCode.getText(), pStreet.getText(),
-                    pNumber.getText(), pPesel.getText());
+            PatientsDAO.insertPatient(pSurname.getText(), pName.getText(), pCity.getText(), pZipCode.getText(),
+                    pStreet.getText(), pNumber.getText(), pPesel.getText());
             cancelPatient();
             Main.showVisits();
             Main.setAddPatientModal(false);
         } else {
-            PatientsDAO.updatePatient(PatientsController.getpId(), pSurname.getText(), pName.getText(), pCity.getText(), pZipCode.getText(), pStreet.getText(),
+            PatientsDAO.updatePatient(PatientsController.getpId(), pSurname.getText(), pName.getText(), pCity.getText(),
+                    pZipCode.getText(), pStreet.getText(),
                     pNumber.getText(), pPesel.getText());
             cancelPatient();
             Main.showVisits();
         }
     }
+        else patientValidator.setText("Proszę wypełnić wszystkie pola!!!");
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-
-//
-//        RequiredFieldValidator validator = new RequiredFieldValidator();
-//        pSurname.getValidators().add(validator);
-//        validator.setMessage("Proszę wprowadzić dane");
-//
-//        pSurname.focusedProperty().addListener((observable, oldValue, newValue) -> {
-//            if(!newValue){
-//                pSurname.validate();
-//            }
-
+        val();
 
     }
 
     private void val() {
-        RequiredFieldValidator validator = new RequiredFieldValidator();
-        pSurname.getValidators().add(validator);
-        validator.setMessage("Proszę wprowadzić dane");
+        RequiredFieldValidator surname_validator = new RequiredFieldValidator();
+        pSurname.getValidators().add(surname_validator);
+        surname_validator.setMessage("Proszę wprowadzić poprawne dane");
 
         pSurname.focusedProperty().addListener((observable, oldValue, newValue) -> {
             if (!newValue) {
+                if (!pSurname.getText().matches("[a-zA-Z]+")) {
+                    pSurname.setText("");
+                }
                 pSurname.validate();
             }
         });
 
+
+        RequiredFieldValidator name_validator = new RequiredFieldValidator();
+        pName.getValidators().add(name_validator);
+        name_validator.setMessage("Proszę wprowadzić poprawne dane");
+
+        pName.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                if (!pName.getText().matches("[a-zA-Z]+")) {
+                    pName.setText("");
+                }
+                pName.validate();
+            }
+        });
+
+        RequiredFieldValidator city_validator = new RequiredFieldValidator();
+        pCity.getValidators().add(city_validator);
+        city_validator.setMessage("Proszę wprowadzić poprawne dane");
+
+        pCity.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                if (!pCity.getText().matches("[a-zA-Z]+")) {
+                    pCity.setText("");
+                }
+                pCity.validate();
+            }
+        });
+
+        RequiredFieldValidator street_validator = new RequiredFieldValidator();
+        pStreet.getValidators().add(street_validator);
+        street_validator.setMessage("Proszę wprowadzić poprawne dane");
+
+        pStreet.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                if (!pStreet.getText().matches("[a-zA-Z]+")) {
+                    pStreet.setText("");
+                }
+                pStreet.validate();
+            }
+        });
+
+        RequiredFieldValidator number_validator = new RequiredFieldValidator();
+        pNumber.getValidators().add(number_validator);
+        number_validator.setMessage("Proszę wprowadzić poprawne dane");
+
+        pNumber.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                if (!pNumber.getText().matches("^[a-zA-Z0-9/]{1,7}$")) {
+                    pNumber.setText("");
+                }
+                pNumber.validate();
+            }
+        });
+
+        RequiredFieldValidator zip_code_validator = new RequiredFieldValidator();
+        pZipCode.getValidators().add(zip_code_validator);
+        zip_code_validator.setMessage("Proszę wprowadzić poprawne dane");
+
+        pZipCode.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                if (!pZipCode.getText().matches("^\\d{2}-\\d{3}$")) {
+                    pZipCode.setText("");
+                }
+                pZipCode.validate();
+            }
+        });
+
+        RequiredFieldValidator pesel_validator = new RequiredFieldValidator();
+        pPesel.getValidators().add(pesel_validator);
+        pesel_validator.setMessage("Proszę wprowadzić poprawne dane");
+
+        pPesel.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                if (!pPesel.getText().matches("^\\d{11}$")) {
+                    pPesel.setText("");
+                }
+                pPesel.validate();
+            }
+        });
+
+
     }
 
-    public void setPatientData(String Surname, String Name, String City, String ZipCode, String Street, String Number, String Pesel) {
+
+    public void setPatientData(String Surname, String Name, String City, String ZipCode, String Street, String Number,
+                               String Pesel) {
         pSurname.setText(Surname);
         pName.setText(Name);
         pCity.setText(City);
@@ -107,4 +190,11 @@ public class AddPatientController implements Initializable {
         pPesel.setText(Pesel);
     }
 
+    private boolean checkPatientFields() {
+
+        return (pSurname.getText().trim().isEmpty() || pName.getText().trim().isEmpty() ||
+                pCity.getText().trim().isEmpty() || pStreet.getText().trim().isEmpty() ||
+                pNumber.getText().trim().isEmpty() || pZipCode.getText().trim().isEmpty() ||
+                pPesel.getText().trim().isEmpty());
+    }
 }
